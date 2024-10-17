9f24f838f42c23148d66ed4631de2dae9ead024c
suzhetao <suzhetao@gmail.com>
2024-10-17 13:39:41 +0800

    modified:   resource.tf

--- resource.tf
+++ resource.tf
@@ -1,11 +1,11 @@
 module "aws_vpc" {
-  source        = "git::http://gitea.home.local/suzhetao/terraform-module-aws.git//vpc?ref=v0.1"
+  source        = "git::https://gitea.home.local/suzhetao/terraform-module-aws.git//vpc?ref=v0.1"
   tags          = var.tags
   aws_resources = var.aws_resources
 }
 
 module "aws_subnet" {
-  source        = "git::http://gitea.home.local/suzhetao/terraform-module-aws.git//subnet?ref=v0.1"
+  source        = "git::https://gitea.home.local/suzhetao/terraform-module-aws.git//subnet?ref=v0.1"
   tags          = var.tags
   aws_resources = var.aws_resources
   depends_on    = [module.aws_vpc]
